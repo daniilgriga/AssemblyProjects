f130d74c85e409a402b1296e7687f4bf2ba90b32
daniilgriga <daniilextra13@gmail.com>
2025-02-08 15:01:14 +0300

    1v BoxText

--- 02Boxes.asm
+++ 02Boxes.asm
@@ -4,39 +4,43 @@ org 100h
 
 Lesgo:      mov bx, 0b800h
             mov es, bx
-            mov bx, 5*80*2 + 20*2
+            mov di, 5*80*2 + 20*2
 
             call BoxBuild
+
+            call BoxText
 
             mov ah, 4ch                 ; exit
             int 21h
 
+BoxSizeX = 20
+BoxSizeY = 10
+
 ;=============================================================================
 ; Draws box
-; Entry:    bx = Box location
+; Entry: bx = Box location
 
 ; Exit:  none
 ; Destr: DI, SI, BX, CX                                                    !!!
 ;=============================================================================
 BoxBuild    proc
 
-            mov di, bx                          ; X = 20, Y = 5
             mov si, offset FrameStyle1          ; addr FrameStyle1 in si
             call BoxLine
 
             add si, 3                           ; go to next trio of symbols
-            mov cx, 5
+            mov cx, BoxSizeY - 2
             next:
                     mov dx, cx                  ; save cx for last loop
 
-                    add di, 80 - 2
+                    add di, 80*2 - BoxSizeX*2 - 2
                     call BoxLine
 
                     mov cx, dx                  ; return cx for last loop
             loop next
 
             add si, 3                           ; go to next trio of symbols
-            add di, 80 - 2
+            add di, 80*2 - BoxSizeX*2 - 2
             call BoxLine
 
             ret
@@ -52,10 +56,10 @@ BoxBuild    proc
 ;=============================================================================
 BoxLine     proc
 
-            mov al, [si]                        ; ASCII in al now
+            mov al, [si]                        ; symbol ASCII in al now
             mov byte ptr es:[di], al            ; write symbol
 
-            mov cx, 40                          ; length of Box
+            mov cx, BoxSizeX                    ; length of Box
             go:
                     add di, 2                   ; skip attribute bite
                     mov al, [si + 1]            ; ASCII in al now
@@ -70,12 +74,50 @@ BoxLine     proc
             endp
 
 ;=============================================================================
+; Write Text in the Box
+; Entry:    cl = length of Text
+;           di = place in video memory
+;           es = segment
+; Exit: none
+; Destr: SI, AX, BX,                                                                    !!!
+;=============================================================================
+BoxText     proc
+
+            mov si, offset Text
+            call StrLen                             ; cl = strlen (Text)
+
+            mov bx, 0
+
+            mov al, cl
+            mov bl, 2
+            mul bl
+
+            mov bx, ax
+
+            add bx, 80*(BoxSizeY - 2) + BoxSizeX/2  ; go to 'center' of Box
+
+            sub di, bx
+
+            string:                                 ; write a Text
+                    mov al, [si]
+                    mov byte ptr es:[di], al
+
+                    add di, 2
+                    add si, 1
+            loop string
+
+            ret
+            endp
+
+;=============================================================================
 ; Count length of string
 ; Entry: bx = string offset
 ; Exit:  cl = length of string
 ; Destr: AL                                                                !!!
 ;=============================================================================
 StrLen      proc
+
+            mov bx, si
 
             mov cl, 0
 
@@ -115,5 +157,7 @@ PutSymbol   proc
 FrameStyle1 db  201, 205, 187, 186, " ", 186, 200, 205, 188, "$"
 ; FrameStyle2 db
 ; FrameStyle3 db
+
+Text        db "stay hard", "#"
 
 end Lesgo
