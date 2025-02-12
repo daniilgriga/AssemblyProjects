e30697073a80dc1614eb8224edb974af6fc11ca8
daniilgriga <daniilextra13@gmail.com>
2025-02-12 23:21:01 +0300

    fixed SizeAnalysis and other upds

--- 02Boxes.asm
+++ 02Boxes.asm
@@ -2,115 +2,120 @@
 .code
 org 100h
 
-Lesgo:      mov bx, 0b800h
-            mov es, bx
-            mov di, 5*80*2 + 20*2
+VIDEOSEG equ 0b800h
 
-            call SizeAnalysis
+Lesgo:          mov bx, VIDEOSEG
+                mov es, bx
+                mov di, 5*80*2 + 20*2
 
-            call BoxBuild
+                call SizeAnalysis
 
-            call BoxText
+                call BoxBuild
 
-            mov ah, 4ch                 ; exit
-            int 21h
+                call BoxText
+
+                mov ah, 4ch                                 ; exit
+                int 21h
 
 BoxSizeX db  20
 BoxSizeY equ 10
 
 ;=============================================================================
-; Draws box
+; Size of Box analysis
 ; Entry:
 ; Exit:
-; Destr:                                                                   !!!
+; Destr: AL, CL                                                            !!!
 ;=============================================================================
 SizeAnalysis    proc
 
-            mov si, offset Text
-            call StrLen                         ; cl = length of Text
+                mov si, offset Text
+                call StrLen                             ; cl = length of Text
 
-            mov al, [BoxSizeX]
-            cmp cl, al                          ; compare
+                mov al, [BoxSizeX]
+                cmp cl, al                              ; compare
 
-            jbe lol                             ; BoxSizeX => length of Text ? nothing : change
+                jbe lol                                 ; BoxSizeX => length of Text ? nothing : change
 
                 mov [BoxSizeX], cl
-                add [BoxSizeX], 4; update size
+                add [BoxSizeX], 4                       ; update size
 
-            lol:
+                lol:
 
-            ret
-            endp
-
+                ret
+                endp
 
 ;=============================================================================
 ; Draws box
-; Entry:    di = location rn
-
+; Entry:        di = location rn
+;
 ; Exit:  none
 ; Destr: DI, SI, BX, CX                                                    !!!
 ;=============================================================================
 BoxBuild    proc
 
-            mov si, offset FrameStyle1          ; addr FrameStyle1 in si
+            mov si, offset FrameStyle2                  ; addr FrameStyle1 in si
             call BoxLine
 
-            add si, 3                           ; go to next trio of symbols
+            add si, 3                                   ; go to next trio of symbols
+
             mov cl, BoxSizeY - 2
-
             next:
-                    mov dx, cx                  ; save cx for last loop
+                    mov dx, cx                          ; save cx for last loop
 
-                    mov al, [BoxSizeX]
-                    mov ah, 2
-                    mul ah
-                    add di, 80*2
-                    sub di, ax
-                    sub di, 2
+                    call NextPosition
+                    call BoxLine                        ; draw body of Box
 
-                    call BoxLine                ; draw body of Box
-
-                    mov cx, dx                  ; return cx for last loop
+                    mov cx, dx                          ; return cx for last loop
             loop next
 
-            add si, 3                           ; go to next trio of symbols
-
-            mov al, [BoxSizeX]
-            mov ah, 2
-            mul ah
-            add di, 80*2
-            sub di, ax
-            sub di, 2
-
+            add si, 3                                   ; go to next trio of symbols
+            call NextPosition
             call BoxLine
 
             ret
             endp
 
 ;=============================================================================
+; Change DI to next position for string
+; Entry:        di = location
+; Exit:         di = update location
+; Destr: AX, DI                                                            !!!
+;=============================================================================
+NextPosition    proc
+
+                mov al, [BoxSizeX]
+                shl ax, 1
+                add di, 80*2
+                sub di, ax
+                sub di, 2
+
+                ret
+                endp
+
+;=============================================================================
 ; Draws string
-; Entry:    di = location
-;           si = framestyle offset
-;           es = segment
+; Entry:        di = location
+;               si = framestyle offset
+;               es = segment
 ;
 ; Exit: none
 ; Destr: AL, DI, SI, CX                                                    !!!
 ;=============================================================================
 BoxLine     proc
 
-            mov al, [si]                        ; symbol ASCII in al now
-            mov byte ptr es:[di], al            ; write symbol
+            mov al, [si]                                ; symbol ASCII in al now
+            mov byte ptr es:[di], al                    ; write symbol
 
-            mov cl, [BoxSizeX]                  ; length of Box
+            mov cl, [BoxSizeX]                          ; length of Box
             go:
-                    add di, 2                   ; skip attribute bite
-                    mov al, [si + 1]            ; ASCII in al now
-                    mov byte ptr es:[di], al    ; write symbol
+                    add di, 2                           ; skip attribute bite
+                    mov al, [si + 1]                    ; ASCII in al now
+                    mov byte ptr es:[di], al            ; write symbol
             loop go
 
-            add di, 2                           ; skip attribute bite
-            mov al, [si + 2]                    ; ASCII in al now
-            mov byte ptr es:[di], al            ; write symbol
+            add di, 2                                   ; skip attribute bite
+            mov al, [si + 2]                            ; ASCII in al now
+            mov byte ptr es:[di], al                    ; write symbol
 
             ret
             endp
@@ -125,34 +130,24 @@ BoxLine     proc
 BoxText     proc
 
             mov si, offset Text
-            call StrLen                             ; cl = strlen (Text)
+            call StrLen                                 ; cl = strlen (Text)
 
-            mov bx, 0
+            xor bx, bx
 
             mov al, cl
-            mov bl, 2
-            mul bl
+            shr al, 1
+            shl al, 1
 
             mov bx, ax
 
-            mov al, [BoxSizeX]                      ; - 34 instead of div 2
-            sub al, 34
+            mov al, [BoxSizeX]
 
-            ;mov ah, 2
-
-            ;cmp ah, 0
-            ;je save_div
-
-            ;div ah
-
-            ;save_div:
-
+            add bx, ax
             add bx, 80*(BoxSizeY - 2)
-            add bx, ax                              ; go to 'center' of Box
 
             sub di, bx
 
-            string:                                 ; write a Text (using cx for loop)
+            string:                                     ; write a Text (using cx for loop)
                     mov al, [si]
                     mov byte ptr es:[di], al
 
@@ -165,7 +160,7 @@ BoxText     proc
 
 ;=============================================================================
 ; Count length of string
-; Entry: bx = string offset
+; Entry: si = string offset
 ; Exit:  cl = length of string
 ; Destr: AL                                                                !!!
 ;=============================================================================
@@ -173,8 +168,7 @@ StrLen      proc
 
             mov bx, si
 
-            mov cl, 0
-
+            xor cl, cl
             cycle:
                     mov al, [bx]
                     cmp al, '#'
@@ -190,27 +184,9 @@ StrLen      proc
                     ret
                     endp
 
-;=============================================================================
-; Draws one char to video memory in (x = 40, y = 5)
-; Entry: cl
-; Exit:  none
-; Destr: BX, ES                                                            !!!
-;=============================================================================
-PutSymbol   proc
-
-            mov bx, 0b800h
-            mov es, bx
-            mov bx, 5*80*2 + 40*2
-
-            mov byte ptr es:[bx], cl
-            mov byte ptr es:[bx + 1], 10011101b
-
-            ret
-            endp
-
 FrameStyle1 db  201, 205, 187, 186, " ", 186, 200, 205, 188
-FrameStyle2 db "+-+| |\_/"
-; FrameStyle3 db
+FrameStyle2 db  "+-+| |\_/"
+FrameStyle3 db    3,   3,   3,   3, " ",   3,   3,   3,   3
 
 Text        db "David Goggins once said: stay hard", "#"
 
